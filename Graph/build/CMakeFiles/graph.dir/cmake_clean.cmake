da321b79cc8a2f2ffbf56b9877ccfefda8546082
lyteen <lyteen4@gmail.com>
2024-12-08 16:44:13 +0800

    Graph-adjacency achieve

---
+++ Graph/build/CMakeFiles/graph.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graph.dir/src/graph.cc.obj"
+  "CMakeFiles/graph.dir/src/graph.cc.obj.d"
+  "CMakeFiles/graph.dir/src/main.cc.obj"
+  "CMakeFiles/graph.dir/src/main.cc.obj.d"
+  "graph.exe"
+  "graph.exe.manifest"
+  "graph.pdb"
+  "libgraph.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
